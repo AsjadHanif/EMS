95a14e7b7475ad08b47ee19c08d48698900e96c8
AsjadHanif <asjad.hanif970@gmail.com>
2024-06-24 17:40:02 +0500

    EMS

---
+++ conn.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "ems";
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+if ($conn) {
+    echo "DataBase connected.";
+}else{
+    die("Connection failed: ". mysqli_connect_error());
+}
+
+?>
