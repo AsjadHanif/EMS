cb25f6e8800af45443d9d01e993a306d575b1455
AsjadHanif <asjad.hanif970@gmail.com>
2024-06-29 15:25:56 +0500

    Update livesearch.php

--- livesearch.php
+++ livesearch.php
@@ -1,9 +1,21 @@
-<?php
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>He</title>
+  </head>
+  <body>
+
+  <?php
 include "conn.php";
+session_start();
 if (isset($_POST['input'])){
 
     $input = $_POST['input'];
-    $querylive = "SELECT * FROM ems";
+    $querylive = "SELECT * FROM ems WHERE user_id LIKE '{$input}%' OR name LIKE '{$input}%' OR pseudo_id LIKE '{$input}%' OR sale LIKE '{$input}%' OR date LIKE '{$input}%' LIMIT 5";
 
     $resultlive = mysqli_query($conn, $querylive) or die("Couldn't connect to");
 
@@ -28,7 +40,6 @@ if (isset($_POST['input'])){
                 $pseudo_id = $row['pseudo_id']; 
                 $sale = $row['sale'];
                 $date = $row['date'];
-            }
             ?>
             <tr>
                 <td><?php echo $id;?></td>
@@ -36,7 +47,18 @@ if (isset($_POST['input'])){
                 <td><?php echo $pseudo_id;?></td>
                 <td><?php echo $sale;?></td>
                 <td><?php echo $date;?></td>
+                <td class="">
+                    <?php
+                    if($_SESSION['role'] == '1'){ ?>
+                    <a class="btn btn-sm btn-danger " id="delBtn" href='delete-user.php?id=<?php echo $row["user_id"]; ?>'>Delete</a>
+                    <?php
+                    }
+                    ?>
+                </td>
             </tr>
+            <?php
+    }
+            ?>
         </tbody>
         </table>
     
@@ -47,3 +69,8 @@ if (isset($_POST['input'])){
 }
 
 ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
